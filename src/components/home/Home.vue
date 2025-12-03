2c017623d6911b4252f3889f41a8bc0e4c9d31a0
Alex Helm <alex.helm@yahoo.com>
2025-12-02 19:28:09 -0500

    Update

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -52,16 +52,16 @@
               direction="horizontal"
             >
               <v-timeline-item 
-                dot-color="blue"
+                :dot-color="SenateMajorityParty"
                 icon="mdi-bank-outline"
               >
                 <template v-slot:opposite>
                   <span>Senate</span>
                 </template>
-                <div>Democratic</div>
+                <div>{{ currentSenate.MajorityParty }}</div>
               </v-timeline-item>
-              <v-timeline-item 
-                dot-color="red"
+              <v-timeline-item
+                class="president-dot"
               >
                 <template v-slot:icon>
                   <v-avatar :image="currentPresident.Image"></v-avatar>
@@ -72,13 +72,13 @@
                 <div>{{ currentPresident.Name }}</div>
               </v-timeline-item>
               <v-timeline-item 
-                dot-color="blue"
+                :dot-color="HouseMajorityParty"
                 icon="mdi-bank-outline"
               >
                 <template v-slot:opposite>
                   <span>House</span>
                 </template>
-                <div>Democratic</div>
+                <div>{{ currentHouse.MajorityParty }}</div>
               </v-timeline-item>
             </v-timeline>
           </v-col>
@@ -89,23 +89,44 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import { RecycleScroller } from 'vue3-virtual-scroller'
 
 var recycleScroll = ref(null)
 var currentPresident = ref(null)
 var currentMonthYear = ref("January 1700")
+var currentSenate = ref(null)
+var currentHouse = ref(null)
+const partyColors = ref({
+  "Federalist": "#e49edd",
+  "Jeffersonian Republican": "#b3e5a1",
+  "Jacksonian": "#5a73d0",
+  "Anti-Jacksonian": "#ffe599",
+  "National Republican": "#ffe599",
+  "Democratic": "#0000ff",
+  "Whig": "#f6c6ac",
+  "Republican": "#ff0000"
+})
 const historyData = ref([])
 
-function render(item) {
+const SenateMajorityParty = computed(() =>
+  partyColors.value[currentSenate.value.MajorityParty] || "#d8d8d8"
+)
+const HouseMajorityParty = computed(() =>
+  partyColors.value[currentHouse.value.MajorityParty] || "#d8d8d8"
+)
+
+function render() {
   var positionArray = []
   Array.from(recycleScroll.value.$_views).forEach(element => {
     positionArray.push(element[1])
   });
   var sortedArray = positionArray.sort((a, b) => a.position - b.position)
   currentMonthYear.value = sortedArray[6].item.MonthName + " " + sortedArray[6].item.Year
-  console.log(sortedArray[6].item.President)
   currentPresident.value = sortedArray[6].item.President
+  currentSenate.value = sortedArray[6].item.Senate
+  currentHouse.value = sortedArray[6].item.House
+  
 }
 
 onMounted(async () => {
@@ -120,9 +141,12 @@ onMounted(async () => {
 .scroller {
   height: 70vh;
 }
-
 .month {
   border-top: 2px solid white;
 }
+/* ::v-deep(.president-dot .v-timeline-divider__dot.v-timeline-divider__dot--size-large) {
+  border: 3px solid #b3e5a1;
+  box-sizing: border-box;
+} */
 </style>
   
